bb03b56aa0dbe54a85e991bc88f78f17abea69e0
msp397 <marim@torus.tech>
2025-02-10 15:53:03 +0530

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:torus_flutter_widgets/material_widgets/widgets/dynamic/datetimepicker.dart';
-import 'package:torus_flutter_widgets/material_widgets/widgets/dynamic/switch.dart';
-import 'package:torus_flutter_widgets/material_widgets/widgets/dynamic/table.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:torus_flutter_widgets/material_widgets/widgets/bloc/connectivity_cubit.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      theme: ThemeData.light(),
+      title: 'Connectivity Monitor',
+      home: BlocProvider(
+        create: (context) => ConnectivityCubit(Connectivity())..monitorConnectivity(),
+        child: HomePage(),
+      ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Dynamic')),
-      body: Center(
-        child: TDateTimePicker(),
+      appBar: AppBar(
+        title: Text('Internet Connectivity'),
       ),
+      body: ConnectivityStatus(),
+    );
+  }
+}
+
+class ConnectivityStatus extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ConnectivityCubit, ConnectivityResult>(
+      listener: (context, connectivityResult) {
+        if (connectivityResult == ConnectivityResult.none) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('No internet connection'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).clearSnackBars();
+        }
+      },
+      child: Center(
+        child: ConnectivityText(),
+      ),
+    );
+  }
+}
+
+class ConnectivityText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ConnectivityCubit, ConnectivityResult>(
+      builder: (context, connectivityResult) {
+        String statusText;
+        Color statusColor;
+
+        if (connectivityResult == ConnectivityResult.none) {
+          statusText = 'No Internet';
+          statusColor = Colors.red;
+        } else {
+          statusText = 'Connected';
+          statusColor = Colors.green;
+        }
+
+        return Text(
+          statusText,
+          style: TextStyle(fontSize: 24, color: statusColor),
+        );
+      },
     );
   }
 }
