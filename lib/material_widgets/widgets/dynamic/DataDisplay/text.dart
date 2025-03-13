e3c2f20de2b33a7d78fbd99fe34e3457fe09f974
rahulsakthi306 <rahuls@torus.tech>
2025-03-13 12:11:52 +0530

    latest chnages

--- lib/material_widgets/widgets/dynamic/DataDisplay/text.dart
+++ lib/material_widgets/widgets/dynamic/DataDisplay/text.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 class CommonTextWidget extends StatelessWidget {
   final String text;
   final TextStyle? textStyle;
+  final FontWeight fontWeight;
   final String textTheme;
+  final TextOverflow textOverflow;
+  final TextAlign textAlign;
 
   const CommonTextWidget({
     super.key,
     required this.text,
     this.textStyle,
     this.textTheme = 'labelMedium',
+    this.textOverflow = TextOverflow.fade,
+    this.textAlign = TextAlign.center,
+    this.fontWeight = FontWeight.normal
   });
 
   @override
@@ -19,6 +25,8 @@ class CommonTextWidget extends StatelessWidget {
 
     return Text(
       text,
+      overflow: textOverflow,
+      textAlign: textAlign,
       style: textStyle ?? themeTextStyle,
     );
   }
@@ -26,37 +34,37 @@ class CommonTextWidget extends StatelessWidget {
   TextStyle _getTextThemeStyle(String theme) {
     switch (theme) {
       case 'displayLarge':
-        return TextStyle(fontSize: 57, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 57, fontWeight: fontWeight);
       case 'displayMedium':
-        return TextStyle(fontSize: 45, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 45, fontWeight: fontWeight);
       case 'displaySmall':
-        return TextStyle(fontSize: 36, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 36, fontWeight: fontWeight);
       case 'headlineLarge':
-        return TextStyle(fontSize: 32, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 32, fontWeight: fontWeight);
       case 'headlineMedium':
-        return TextStyle(fontSize: 28, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 28, fontWeight: fontWeight);
       case 'headlineSmall':
-        return TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 24, fontWeight: fontWeight);
       case 'titleLarge':
-        return TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 22, fontWeight: fontWeight);
       case 'titleMedium':
-        return TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 16, fontWeight: fontWeight);
       case 'titleSmall':
-        return TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+        return TextStyle(fontSize: 14, fontWeight: fontWeight);
       case 'labelLarge':
-        return TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 14, fontWeight: fontWeight);
       case 'labelMedium':
-        return TextStyle(fontSize: 12, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 12, fontWeight: fontWeight);
       case 'labelSmall':
-        return TextStyle(fontSize: 11, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 11, fontWeight: fontWeight);
       case 'bodyLarge':
-        return TextStyle(fontSize: 16, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 16, fontWeight: fontWeight);
       case 'bodyMedium':
-        return TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 14, fontWeight: fontWeight);
       case 'bodySmall':
-        return TextStyle(fontSize: 12, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 12, fontWeight: fontWeight);
       default:
-        return TextStyle(fontSize: 12, fontWeight: FontWeight.normal);
+        return TextStyle(fontSize: 12, fontWeight: fontWeight);
     }
   }
 }
