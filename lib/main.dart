89e5ae6de1ea2447deac5c6379d905bcafbed37a
rahulsakthi306 <rahuls@torus.tech>
2025-03-14 10:05:10 +0530

    latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:torus_flutter_widgets/material_widgets/widgets/customwidget/fab.dart';
-import 'package:torus_flutter_widgets/pinInput_new.dart';
-import 'package:torus_flutter_widgets/pinin.dart';
+import 'package:torus_flutter_widgets/material_widgets/widgets/dynamic/Navigation/tab_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,31 +9,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Flexible(
-                    flex: 12,
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: PinInputCustom()
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TabBar'),
       ),
+      body: CommonTabBar(tabs: []),
     );
   }
 }
